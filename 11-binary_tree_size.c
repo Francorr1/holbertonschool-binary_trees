81df68f0ab3d21ad0fdccc5e9de81fd1a75f4cce
root <gonzalopeder2@gmail.com>
2023-05-15 11:39:50 -0700

    Task 11 completed, working and Betty style corrected

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,25 +1,23 @@
 #include "binary_trees.h"
 /**
- * 
- * 
+ * binary_tree_size - calculates the size of a tree
+ *
+ * @tree: a tree
+ *
+ * Return: size or 0 if no tree
 */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-    size_t size = 0;
-    if (tree != NULL)
-    {
-        size += binary_tree_size(tree->left);
-        if (tree->left != NULL)
-        {
-            return (size + 1);
-        }
-        
-        size += binary_tree_size(tree->right);
-        if (tree->right != NULL)
-        {
-            return(size + 1);
-        }
-        return (size);
-    }
-    return(size);
+	size_t left = 0, right = 0;
+
+	if (tree != NULL)
+	{
+		left = binary_tree_size(tree->left);
+		right = binary_tree_size(tree->right);
+		return ((left + right) + 1);
+	}
+	else
+	{
+		return (0);
+	}
 }
