2211419c6c9de4d5614b7a56d03e914a4c6ecac0
root <gonzalopeder2@gmail.com>
2023-05-15 11:51:51 -0700

    Header update

--- binary_trees.h
+++ binary_trees.h
@@ -1,8 +1,11 @@
 #ifndef _BINARY_TREES_H_
 #define _BINARY_TREES_H_
+/*Libraries*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
+
+/*Structures*/
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -21,10 +24,12 @@ struct binary_tree_s
 
 typedef struct binary_tree_s binary_tree_t;
 
+/*Prototypes*/
 void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+void binary_tree_delete(binary_tree_t *tree);
 int binary_tree_is_leaf(const binary_tree_t *node);
 int binary_tree_is_root(const binary_tree_t *node);
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
@@ -32,6 +37,6 @@ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 size_t binary_tree_height(const binary_tree_t *tree);
 size_t binary_tree_depth(const binary_tree_t *tree);
-void binary_tree_delete(binary_tree_t *tree);
 size_t binary_tree_size(const binary_tree_t *tree);
+
 #endif
