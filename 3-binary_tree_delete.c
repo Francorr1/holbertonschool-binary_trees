179ee11b06a235cd08cdeabe923dab23dbb00f27
root <gonzalopeder2@gmail.com>
2023-05-15 14:04:55 -0700

    Task 3 errors corrected

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -6,18 +6,10 @@
 */
 void binary_tree_delete(binary_tree_t *tree)
 {
-	if (tree != NULL && (tree->left != NULL || tree->right != NULL))
+	if (tree != NULL)
 	{
 		binary_tree_delete(tree->left);
-		if (tree->parent != NULL)
-		{
-		free(tree->left);
-		}
 		binary_tree_delete(tree->right);
-		if (tree->parent != NULL)
-		{
-		free(tree->right);
-		}
-		free(tree);
 	}
+	free(tree);
 }
